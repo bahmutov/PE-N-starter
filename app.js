c752c3a242b8cd3abf8facdb1c2b99ae70bf5a92
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-04-12 20:29:11 -0400

    fixed account delete

--- app.js
+++ app.js
@@ -95,7 +95,6 @@ app.get('/contact', contactController.getContact);
 app.post('/contact', contactController.postContact);
 app.get('/account', passportConf.isAuthenticated, userController.getAccount);
 app.post('/account/profile', passportConf.isAuthenticated, userController.postUpdateProfile);
-app.post('/account/password', passportConf.isAuthenticated, userController.postUpdatePassword);
 app.post('/account/delete', passportConf.isAuthenticated, userController.postDeleteAccount);
 
 /**
--- controllers/user.js
+++ controllers/user.js
@@ -3,8 +3,9 @@ var async = require('async');
 var crypto = require('crypto');
 var nodemailer = require('nodemailer');
 var passport = require('passport');
-var User = require('../models/User');
+var User = require('../models/User')();
 var config = require('../config');
+var check = require('check-more-types');
 
 /**
  * GET /login
@@ -56,54 +57,6 @@ exports.logout = function(req, res) {
 };
 
 /**
- * GET /signup
- * Signup page.
- */
-exports.getSignup = function(req, res) {
-  if (req.user) return res.redirect('/');
-  res.render('account/signup', {
-    title: 'Create Account'
-  });
-};
-
-/**
- * POST /signup
- * Create a new local account.
- */
-exports.postSignup = function(req, res, next) {
-  console.log('post signup', req);
-  req.assert('email', 'Email is not valid').isEmail();
-  req.assert('password', 'Password must be at least 4 characters long').len(4);
-  req.assert('confirmPassword', 'Passwords do not match').equals(req.body.password);
-
-  var errors = req.validationErrors();
-
-  if (errors) {
-    req.flash('errors', errors);
-    return res.redirect('/signup');
-  }
-
-  var user = new User({
-    email: req.body.email,
-    password: req.body.password
-  });
-
-  User.findOne({ email: req.body.email }, function(err, existingUser) {
-    if (existingUser) {
-      req.flash('errors', { msg: 'Account with that email address already exists.' });
-      return res.redirect('/signup');
-    }
-    user.save(function(err) {
-      if (err) return next(err);
-      req.logIn(user, function(err) {
-        if (err) return next(err);
-        res.redirect('/');
-      });
-    });
-  });
-};
-
-/**
  * GET /account
  * Profile page.
  */
@@ -135,44 +88,18 @@ exports.postUpdateProfile = function(req, res, next) {
 };
 
 /**
- * POST /account/password
- * Update current password.
- */
-exports.postUpdatePassword = function(req, res, next) {
-  req.assert('password', 'Password must be at least 4 characters long').len(4);
-  req.assert('confirmPassword', 'Passwords do not match').equals(req.body.password);
-
-  var errors = req.validationErrors();
-
-  if (errors) {
-    req.flash('errors', errors);
-    return res.redirect('/account');
-  }
-
-  User.findById(req.user.id, function(err, user) {
-    if (err) return next(err);
-
-    user.password = req.body.password;
-
-    user.save(function(err) {
-      if (err) return next(err);
-      req.flash('success', { msg: 'Password has been changed.' });
-      res.redirect('/account');
-    });
-  });
-};
-
-/**
  * POST /account/delete
  * Delete user account.
  */
 exports.postDeleteAccount = function(req, res, next) {
-  User.remove({ _id: req.user.id }, function(err) {
-    if (err) return next(err);
-    req.logout();
-    req.flash('info', { msg: 'Your account has been deleted.' });
-    res.redirect('/');
-  });
+  la(check.object(req.user), 'missing user to detele');
+  req.user.remove()
+    .then(function () {
+      req.logout();
+      req.flash('info', { msg: 'Your account has been deleted.' });
+      res.redirect('/');
+    })
+    .catch(next);
 };
 
 /**
