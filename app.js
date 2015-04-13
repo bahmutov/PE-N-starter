b2c8a5d387ce5b55c4a3292561ef5e962b10be7b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-04-13 16:21:22 -0400

    refactored out server creation

--- app.js
+++ app.js
@@ -10,26 +10,6 @@ var pouch = new PouchDb(config.get('db'));
 require('./models/init-models')(pouch);
 
 var passportConf = require('./config/passport');
-/**
- * Module dependencies.
- */
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var compress = require('compression');
-var session = require('express-session');
-var FileStore = require('session-file-store')(session);
-var bodyParser = require('body-parser');
-var logger = require('morgan');
-var errorHandler = require('errorhandler');
-var lusca = require('lusca');
-var methodOverride = require('method-override');
-var multer  = require('multer');
-
-var flash = require('express-flash');
-var path = require('path');
-var passport = require('passport');
-var expressValidator = require('express-validator');
-var connectAssets = require('connect-assets');
 
 /**
  * Controllers (route handlers).
@@ -39,51 +19,8 @@ var userController = require('./controllers/user');
 var apiController = require('./controllers/api');
 var contactController = require('./controllers/contact');
 
-/**
- * Create Express server.
- */
-var app = express();
-
-/**
- * Express configuration.
- */
-app.set('port', config.get('PORT'));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(compress());
-app.use(connectAssets({
-  paths: [path.join(__dirname, 'public/css'), path.join(__dirname, 'public/js')]
-}));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(multer({ dest: path.join(__dirname, 'uploads') }));
-app.use(expressValidator());
-app.use(methodOverride());
-app.use(cookieParser());
-app.use(session({
-  resave: true,
-  saveUninitialized: true,
-  secret: config.get('sessionSecret'),
-  store: new FileStore()
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-app.use(lusca({
-  csrf: true,
-  xframe: 'SAMEORIGIN',
-  xssProtection: true
-}));
-app.use(function(req, res, next) {
-  res.locals.user = req.user;
-  next();
-});
-app.use(function(req, res, next) {
-  if (/api/i.test(req.path)) req.session.returnTo = req.path;
-  next();
-});
-app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+var app = require('./config/server');
+var passport = require('passport');
 
 /**
  * Primary app routes.
@@ -113,6 +50,7 @@ app.get('/auth/github/callback', passport.authenticate('github', { failureRedire
 /**
  * Error Handler.
  */
+var errorHandler = require('errorhandler');
 app.use(errorHandler());
 
 /**
--- models/User.js
+++ models/User.js
@@ -14,7 +14,6 @@ function userGravatar(size) {
 };
 
 function createUserStore(pouchDb) {
-  console.log('createUserStore');
   la(pouchDb, 'expected pouch db');
   la(check.fn(pouchDb.store), 'missing pouch store function, use store.pouchdb');
   var User = pouchDb.store();
@@ -25,55 +24,3 @@ function createUserStore(pouchDb) {
 }
 
 module.exports = R.once(createUserStore);
-
-/*
-var userSchema = new mongoose.Schema({
-  email: { type: String, unique: true, lowercase: true },
-  password: String,
-
-  facebook: String,
-  twitter: String,
-  google: String,
-  github: String,
-  instagram: String,
-  linkedin: String,
-  tokens: Array,
-
-  profile: {
-    name: { type: String, default: '' },
-    gender: { type: String, default: '' },
-    location: { type: String, default: '' },
-    website: { type: String, default: '' },
-    picture: { type: String, default: '' }
-  },
-
-  resetPasswordToken: String,
-  resetPasswordExpires: Date
-});
-
-// Password hash middleware.
-userSchema.pre('save', function(next) {
-  var user = this;
-  if (!user.isModified('password')) return next();
-  bcrypt.genSalt(10, function(err, salt) {
-    if (err) return next(err);
-    bcrypt.hash(user.password, salt, null, function(err, hash) {
-      if (err) return next(err);
-      user.password = hash;
-      next();
-    });
-  });
-});
-
-// Helper method for validating user's password.
-userSchema.methods.comparePassword = function(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-    if (err) return cb(err);
-    cb(null, isMatch);
-  });
-};
-
-
-
-module.exports = mongoose.model('User', userSchema);
-*/
