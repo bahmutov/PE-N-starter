a585831c208e48ce3c1304a9eaad48a5f5decce6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-04-13 10:35:03 -0400

    using only the defaults file

--- config/index.js
+++ config/index.js
@@ -5,10 +5,5 @@ nconf.env().argv();
 nconf.file('local', full('local-dev.json'));
 nconf.file('default', full('default-dev.json'));
 
-nconf.defaults({
-  PORT: process.env.PORT || 3000,
-  NODE_ENV: 'development'
-});
-
 module.exports = nconf;
 
